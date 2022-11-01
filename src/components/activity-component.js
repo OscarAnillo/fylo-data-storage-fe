280df2c9db881edf635ac09b7edb9d6c4a739479
oscar anillo <oscarani14@gmail.com>
2022-10-31 21:55:55 -0500

    add animation

--- src/components/activity-component.js
+++ src/components/activity-component.js
@@ -1,21 +1,23 @@
+import 'animate.css';
+
 export default function ActivityComponent(){
     return (
         <section className="activity">
             <div className="first-div">
-                <img src="images/logo.svg" alt="" className="logo" />
+                <img src="images/logo.svg" alt="" className="logo animate__animated animate__backInDown" />
                 <div>
-                    <img src="images/icon-document.svg" alt="" className="document" />
-                    <img src="images/icon-folder.svg" alt="" className="folder"/>
-                    <img src="images/icon-upload.svg" alt="" className="upload"/>
+                    <img src="images/icon-document.svg" alt="" className="document animate__animated animate__backInLeft" />
+                    <img src="images/icon-folder.svg" alt="" className="folder animate__animated animate__backInUp"/>
+                    <img src="images/icon-upload.svg" alt="" className="upload animate__animated animate__backInRight"/>
                 </div>
             </div>
             <div className="second-div">
                 <div>
-                    <p className="used">You've used <span>815 GB</span> of your storage</p>
+                    <p className="used animate__animated animate__backInDown">You've used <span>815 GB</span> of your storage</p>
                     <div className="ball"></div>
                     <progress max="100" value="70">
                     </progress>
-                    <div className="row-second">
+                    <div className="row-second animate__animated animate__backInUp">
                         <div>
                             <p>0 GB</p>
                         </div>
